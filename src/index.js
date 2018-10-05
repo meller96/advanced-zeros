28e650de1f2690aae04142328256c7c2c0169bc0
meller96 <meller70@gmail.com>
2018-10-05 18:25:38 +0300

    advanced-zeros complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let arr = [];
+    let zeros = 0;
+    let j = 0;
+    let i = 2;
+    if (base === 2 ){
+        return 0;
+    }
+    do {
+        if (base % i === 0) {
+            arr[j] = i;
+            j++;
+            base = base / i;
+        }
+        else {
+            i++;
+        }
+    }
+    while (i < base);{
+    arr[j] = i;
+    }
+    let num = 0;
+    for (let k = 0; k < arr.length; k++) {
+        if (arr[k] === base) {
+            num++;
+        }
+
+    }
+
+    for (let k = base; number / k >= 1; k = k * base) {
+        zeros = zeros + Math.floor(number / k);
+    }
+
+    return Math.floor(zeros / num);
+};
